9a36b4b0376001fe3e62ca1420648ead65ebc9f4
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-12-22 08:29:52 -0300

    added and configuration static files.

--- alurarecipes/settings.py
+++ alurarecipes/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-
+            os.path.join(BASE_DIR, 'recipes/templates'), # added recipes apps templates
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -121,4 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'alurarecipes/static')
+]
