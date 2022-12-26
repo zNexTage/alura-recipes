dc6ae2e93a20f0834711a9a06a73798b63f41f92
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-12-26 12:52:21 -0300

    database configuration.
    we're gonna to use postgresql.

--- alurarecipes/settings.py
+++ alurarecipes/settings.py
@@ -11,19 +11,25 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# load and config env vars.
+env = environ.Env()
+
+# find the env file in root folders;
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^6ykw++2&ph93yca_hjc+9sruzra(s59^vg&6ykarq(!=z&*d6'
+SECRET_KEY = env('ALURA_RECIPE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('ALURA_RECIPE_DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -75,11 +81,15 @@ WSGI_APPLICATION = 'alurarecipes.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+print(env('ALURA_RECIPE_DB_NAME'))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('ALURA_RECIPE_DB_NAME'),
+        'USER': os.environ.get('ALURA_RECIPE_USERNAME'),
+        'PASSWORD': os.environ.get('ALURA_RECIPE_PASSWORD'),
+        'HOST': os.environ.get('ALURA_RECIPE_HOST'),
     }
 }
 
